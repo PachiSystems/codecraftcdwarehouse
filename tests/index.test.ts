47e09ae62554d36c57753ee60f5d3f7fc04e5f4e
Brian Milton <PachiSystems@users.noreply.github.com>
2022-09-06 09:34:19 +0100

    feat: basic CD implemented

--- tests/index.test.ts
+++ tests/index.test.ts
@@ -1,10 +1,22 @@
-class CD {
-    constructor(public artist: string, public title: string, public stockLevel: number, public price: number) {
+type CompactDisc = {
+    title: string;
+    artist: string;
+    stock: number;
+    price: number;
+}
 
-    }
+class Warehouse {
+    constructor(public stockList: number[]) {}
+
+}
+
+class CD {
+    constructor(public artist: string, public title: string, public stock: number, public price: number) {}
 
     buy() {
-        this.stockLevel--;
+        if(this.stock > 0) {
+            this.stock--;
+        }
     }
 }
 
@@ -12,6 +24,11 @@ describe('Compact Disc', () => {
     it('should be able to buy a CD', () => {
         const cd = new CD('The Beatles', 'Abbey Road', 10, 9.99);
         cd.buy();
-        expect(cd.stockLevel).toBe(9);
+        expect(cd.stock).toBe(9);
+    });
+    it('should not be able to buy a CD if there are none left', () => {
+        const cd = new CD('The Beatles', 'Abbey Road', 0, 9.99);
+        cd.buy();
+        expect(cd.stock).toBe(0);
     });
 })
